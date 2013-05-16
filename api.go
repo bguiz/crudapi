d3c50de96d9d21ddb883fb6c6b7bb2d07f9587c2
Brendan Graetz <brendan.graetz@gmail.com>
2013-05-16 22:10:51 +1000

    =BG= refactor - move implementation to no auth api methods

--- api.go
+++ api.go
@@ -8,10 +8,7 @@ An example can be found at: https://github.com/sauerbraten/crudapi#example
 package crudapi
 
 import (
-	"encoding/json"
 	"github.com/gorilla/mux"
-	"log"
-	"net/http"
 )
 
 type apiResponse struct {
@@ -20,182 +17,24 @@ type apiResponse struct {
 	Result interface{} `json:"result,omitempty"`
 }
 
-var s Storage
-
 // Adds CRUD and OPTIONS routes to the router, which rely on the given Storage.
-func MountAPI(router *mux.Router, storage Storage) {
-	s = storage
+func MountAPI(router *mux.Router, api ApiMethods) {
 
 	// Create
-	router.HandleFunc("/{kind}", create).Methods("POST")
+	router.HandleFunc("/{kind}", api.CreateOne).Methods("POST")
 
 	// Read
-	router.HandleFunc("/{kind}", getAll).Methods("GET")
-	router.HandleFunc("/{kind}/{id}", get).Methods("GET")
+	router.HandleFunc("/{kind}", api.ReadAll).Methods("GET")
+	router.HandleFunc("/{kind}/{id}", api.ReadOne).Methods("GET")
 
 	// Update
-	router.HandleFunc("/{kind}/{id}", update).Methods("PUT")
+	router.HandleFunc("/{kind}/{id}", api.UpdateOne).Methods("PUT")
 
 	// Delete
-	router.HandleFunc("/{kind}", delAll).Methods("DELETE")
-	router.HandleFunc("/{kind}/{id}", del).Methods("DELETE")
+	router.HandleFunc("/{kind}", api.DeleteAll).Methods("DELETE")
+	router.HandleFunc("/{kind}/{id}", api.DeleteOne).Methods("DELETE")
 
-	// OPTIONS routes for API discovery
-	router.HandleFunc("/{kind}", optionsKind).Methods("OPTIONS")
-	router.HandleFunc("/{kind}/{id}", optionsId).Methods("OPTIONS")
-}
-
-func create(resp http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	kind := vars["kind"]
-	enc := json.NewEncoder(resp)
-	dec := json.NewDecoder(req.Body)
-
-	// read body and parse into interface{}
-	var resource map[string]interface{}
-	err := dec.Decode(&resource)
-
-	if err != nil {
-		log.Println(err)
-
-		resp.WriteHeader(http.StatusBadRequest)
-		err = enc.Encode(apiResponse{"malformed json", "", nil})
-		if err != nil {
-			log.Println(err)
-		}
-
-		return
-	}
-
-	// set in storage
-	id, stoResp := s.Create(kind, resource)
-
-	// write response
-	resp.WriteHeader(stoResp.StatusCode)
-	err = enc.Encode(apiResponse{stoResp.Err, id, nil})
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func getAll(resp http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	kind := vars["kind"]
-	enc := json.NewEncoder(resp)
-
-	// look for resources
-	resources, stoResp := s.GetAll(kind)
-
-	// write response
-	resp.WriteHeader(stoResp.StatusCode)
-	err := enc.Encode(apiResponse{stoResp.Err, "", resources})
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func get(resp http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	kind := vars["kind"]
-	id := vars["id"]
-	enc := json.NewEncoder(resp)
-
-	// look for resource
-	resource, stoResp := s.Get(kind, id)
-
-	// write response
-	resp.WriteHeader(stoResp.StatusCode)
-	err := enc.Encode(apiResponse{stoResp.Err, "", resource})
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func update(resp http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	kind := vars["kind"]
-	id := vars["id"]
-	enc := json.NewEncoder(resp)
-	dec := json.NewDecoder(req.Body)
-
-	// read body and parse into interface{}
-	var resource map[string]interface{}
-	err := dec.Decode(&resource)
-
-	if err != nil {
-		log.Println(err)
-		resp.WriteHeader(http.StatusBadRequest)
-		err = enc.Encode(apiResponse{"malformed json", "", nil})
-		if err != nil {
-			log.Println(err)
-		}
-
-		return
-	}
-
-	// update resource
-	stoResp := s.Update(kind, id, resource)
-
-	// write response
-	resp.WriteHeader(stoResp.StatusCode)
-	err = enc.Encode(apiResponse{stoResp.Err, "", nil})
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-// delete() is a built-in function for maps, thus the shorthand name 'del' for this handler function
-func del(resp http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	kind := vars["kind"]
-	id := vars["id"]
-	enc := json.NewEncoder(resp)
-
-	// delete resource
-	stoResp := s.Delete(kind, id)
-
-	// write response
-	resp.WriteHeader(stoResp.StatusCode)
-	err := enc.Encode(apiResponse{stoResp.Err, "", nil})
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func delAll(resp http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	kind := vars["kind"]
-	enc := json.NewEncoder(resp)
-
-	// look for resources
-	stoResp := s.DeleteAll(kind)
-
-	// write response
-	resp.WriteHeader(stoResp.StatusCode)
-	err := enc.Encode(apiResponse{stoResp.Err, "", nil})
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func optionsKind(resp http.ResponseWriter, req *http.Request) {
-	h := resp.Header()
-
-	h.Add("Allow", "POST")
-	h.Add("Allow", "GET")
-	h.Add("Allow", "DELETE")
-	h.Add("Allow", "OPTIONS")
-
-	resp.WriteHeader(http.StatusOK)
-}
-
-func optionsId(resp http.ResponseWriter, req *http.Request) {
-	h := resp.Header()
-
-	h.Add("Allow", "PUT")
-	h.Add("Allow", "GET")
-	h.Add("Allow", "DELETE")
-	h.Add("Allow", "OPTIONS")
-
-	resp.WriteHeader(http.StatusOK)
+	// Options routes for API discovery
+	router.HandleFunc("/{kind}", api.OptionsAll).Methods("OPTIONS")
+	router.HandleFunc("/{kind}/{id}", api.OptionsOne).Methods("OPTIONS")
 }
--- noauthapimethods.go
+++ noauthapimethods.go
@@ -16,29 +16,162 @@ func NewNoAuthApiMethods(store Storage) NoAuthApiMethods {
 }
 
 func (self NoAuthApiMethods) CreateOne(resp http.ResponseWriter, req *http.Request) {
+  vars := mux.Vars(req)
+  kind := vars["kind"]
+  enc := json.NewEncoder(resp)
+  dec := json.NewDecoder(req.Body)
+
+  // read body and parse into interface{}
+  var resource map[string]interface{}
+  err := dec.Decode(&resource)
+
+  if err != nil {
+   log.Println(err)
+
+   resp.WriteHeader(http.StatusBadRequest)
+   err = enc.Encode(apiResponse{"malformed json", "", nil})
+   if err != nil {
+     log.Println(err)
+   }
+
+   return
+  }
+
+  // set in storage
+  id, stoResp := self.s.Create(kind, resource)
+
+  // write response
+  resp.WriteHeader(stoResp.StatusCode)
+  err = enc.Encode(apiResponse{stoResp.Err, id, nil})
+  if err != nil {
+   log.Println(err)
+  }
+  return
 }
 
 func (self NoAuthApiMethods) ReadOne(resp http.ResponseWriter, req *http.Request) {
+  vars := mux.Vars(req)
+  kind := vars["kind"]
+  id := vars["id"]
+  enc := json.NewEncoder(resp)
+
+  // look for resource
+  resource, stoResp := self.s.Get(kind, id)
+
+  // write response
+  resp.WriteHeader(stoResp.StatusCode)
+  err := enc.Encode(apiResponse{stoResp.Err, "", resource})
+  if err != nil {
+   log.Println(err)
+  }
   return
 }
+
 func (self NoAuthApiMethods) ReadAll(resp http.ResponseWriter, req *http.Request) {
+  vars := mux.Vars(req)
+  kind := vars["kind"]
+  enc := json.NewEncoder(resp)
+
+  // look for resources
+  resources, stoResp := self.s.GetAll(kind)
+
+  // write response
+  resp.WriteHeader(stoResp.StatusCode)
+  err := enc.Encode(apiResponse{stoResp.Err, "", resources})
+  if err != nil {
+   log.Println(err)
+  }
   return
 }
 
 func (self NoAuthApiMethods) UpdateOne(resp http.ResponseWriter, req *http.Request) {
+  vars := mux.Vars(req)
+  kind := vars["kind"]
+  id := vars["id"]
+  enc := json.NewEncoder(resp)
+  dec := json.NewDecoder(req.Body)
+
+  // read body and parse into interface{}
+  var resource map[string]interface{}
+  err := dec.Decode(&resource)
+
+  if err != nil {
+   log.Println(err)
+   resp.WriteHeader(http.StatusBadRequest)
+   err = enc.Encode(apiResponse{"malformed json", "", nil})
+   if err != nil {
+     log.Println(err)
+   }
+
+   return
+  }
+
+  // update resource
+  stoResp := self.s.Update(kind, id, resource)
+
+  // write response
+  resp.WriteHeader(stoResp.StatusCode)
+  err = enc.Encode(apiResponse{stoResp.Err, "", nil})
+  if err != nil {
+   log.Println(err)
+  }
   return
 }
 
 func (self NoAuthApiMethods) DeleteOne(resp http.ResponseWriter, req *http.Request) {
+  vars := mux.Vars(req)
+  kind := vars["kind"]
+  id := vars["id"]
+  enc := json.NewEncoder(resp)
+
+  // delete resource
+  stoResp := self.s.Delete(kind, id)
+
+  // write response
+  resp.WriteHeader(stoResp.StatusCode)
+  err := enc.Encode(apiResponse{stoResp.Err, "", nil})
+  if err != nil {
+   log.Println(err)
+  }
   return
 }
+
 func (self NoAuthApiMethods) DeleteAll(resp http.ResponseWriter, req *http.Request) {
+  vars := mux.Vars(req)
+  kind := vars["kind"]
+  enc := json.NewEncoder(resp)
+
+  // look for resources
+  stoResp := self.s.DeleteAll(kind)
+
+  // write response
+  resp.WriteHeader(stoResp.StatusCode)
+  err := enc.Encode(apiResponse{stoResp.Err, "", nil})
+  if err != nil {
+   log.Println(err)
+  }
   return
 }
 
 func (self NoAuthApiMethods) OptionsOne(resp http.ResponseWriter, req *http.Request) {
+  h := resp.Header()
+
+  h.Add("Allow", "PUT")
+  h.Add("Allow", "GET")
+  h.Add("Allow", "DELETE")
+  h.Add("Allow", "OPTIONS")
+
+  resp.WriteHeader(http.StatusOK)
   return
 }
 func (self NoAuthApiMethods) OptionsAll(resp http.ResponseWriter, req *http.Request) {
+  h := resp.Header()
+
+  h.Add("Allow", "POST")
+  h.Add("Allow", "GET")
+  h.Add("Allow", "DELETE")
+  h.Add("Allow", "OPTIONS")
+
+  resp.WriteHeader(http.StatusOK)
   return
 }
